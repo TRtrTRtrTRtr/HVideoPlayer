e6505127f415fe94941815c45c4e79deda6cfd8f
h js <hjs@hjsdeimac.local>
2016-11-22 11:00:37 +0800

    项目初始化

---
+++ HVideoPlayer/HVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HVideoPlayer
+//
+//  Created by hejunsong on 16/11/22.
+//  Copyright © 2016年 hjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HVideoPlayer/HVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HVideoPlayer
+//
+//  Created by hejunsong on 16/11/22.
+//  Copyright © 2016年 hjs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
